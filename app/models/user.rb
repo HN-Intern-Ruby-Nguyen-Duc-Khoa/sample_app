45ce881f3ed0ae4074e550159036dc4e4efb8d98
[HN_INTERN_Ruby]Nguyen Duc Khoa <nguyen.duc.khoa@sun-asterisk.com>
2022-07-07 14:52:40 +0700

    chapter 8
    Chapter 7
    
    chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,28 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  before_save :downcase_email
+
+  validates :name, presence: true, length:
+  {maximum: 10, too_long: "Tên quá dài"}
+
+  validates :email, presence: true,
+  length: {
+    minium: 20,
+    maximum: 255,
+    too_long: "email qua dai",
+    too_short: "email qua ngan"
+  },
+  format: {with: VALID_EMAIL_REGEX},
+  uniqueness: {case_sensitive: false}
+
+  validates :password, presence: true, length:
+  {minimum: 6}, if: :password
+
+  has_secure_password
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
     root "static_page#home"
     get "/home", to:"static_page#home"
     get "/help", to: "static_page#help"
+    resources :users
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    get "/login", to: "session#new"
+    post "/login", to: "session#create"
+    get "/logout", to: "session#destroy"
   end
 end

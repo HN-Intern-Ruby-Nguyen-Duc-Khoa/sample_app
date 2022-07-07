348483dc0cfc6b581122220955edf85aa5cc234d
[HN_INTERN_Ruby]Nguyen Duc Khoa <nguyen.duc.khoa@sun-asterisk.com>
2022-07-07 14:52:40 +0700

    chapter_10

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem "config"
 gem "htmlbeautifier"
 gem "jbuilder", "~> 2.7"
 gem "mysql2", "~> 0.5"
+gem "pagy"
 gem "puma", "~> 5.0"
 gem "rails", "~> 6.1.6"
 gem "rails-i18n"
@@ -21,7 +22,9 @@ group :development, :test do
 end
 
 group :development do
+  gem "faker", "2.1.2"
   gem "listen", "~> 3.3"
+  gem "pry-rails"
   gem "rack-mini-profiler", "~> 2.0"
   gem "spring"
   gem "web-console", ">= 4.1.0"
@@ -29,7 +32,6 @@ end
 
 group :test do
   gem "capybara", ">= 3.26"
-  gem "pry-rails"
   gem "selenium-webdriver", ">= 4.0.0.rc1"
   gem "webdrivers"
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
-  USER_PARAMS = %i(name email password password_confirmation).freeze
+  before_action :logged_in_user, except: %i(create new index)
+  before_action :load_user, except: %i(create new index)
+  before_action :correct_user, only: %i(edit update)
+
+  def index
+    @pagy, @users = pagy(User.all.order_by_name, items: Settings.users.per_page)
+  end
 
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find_by id: params[:id]
     return if @user
 
     flash[:danger] = t ".show_user_failed"
@@ -24,7 +29,42 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t ".update_successfully"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".update_failed"
+      render :edit
+    end
+  end
+
+  def destroy
+    if user.destroy
+      flash[:success] = t ".success"
+    else
+      flash[:danger] = t ".danger"
+    end
+    redirect_to users_path
+  end
+
   private
+
+  def correct_user
+    return if current_user? @user
+
+    flash[:danger] = t ".edit_correct_danger"
+    redirect_to root_url
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:info] = I18n.t "users.load_user.error"
+  end
 
   def user_params
     params.require(:user).permit USER_PARAMS

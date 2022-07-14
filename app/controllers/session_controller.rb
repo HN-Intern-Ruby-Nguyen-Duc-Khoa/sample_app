9ed23d8dbeffa4e048542f0b9489368d13316d15
[HN_INTERN_Ruby]Nguyen Duc Khoa <nguyen.duc.khoa@sun-asterisk.com>
2022-07-14 13:05:13 +0700

    Chapter 9

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,6 +5,8 @@ class SessionController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
+      # checkbox
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = I18n.t "controller.session_controller.danger_text"
@@ -15,5 +17,12 @@ class SessionController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  def remember user
+    user.remember_me
+    # lưu vào cookies
+    cookies[:remember_token] = { value: user.remember_token, expires: Settings.expires_token.years.from_now.utc }
+    cookies.signed[:user_id] = { value: user.id, expires: Settings.expires_token.years.from_now.utc }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   before_save :downcase_email
 
+  attr_accessor :remember_token
+
+
   validates :name, presence: true, length:
   {maximum: 10, too_long: "Tên quá dài"}
 
@@ -19,6 +22,39 @@ class User < ApplicationRecord
   {minimum: 6}, if: :password
 
   has_secure_password
+
+  class << self
+    # hash fn
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    #random number
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  #lưu token đã được hash vào db
+  def remember_me
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def forget
+    update remember_digest: nil
+  end
+
+  def authenticated? remember_token
+    return false unless remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
 
   private
 
